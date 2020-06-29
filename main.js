5b964dcd345f4ecbd892bd5b94a676105326d6ff
Joshua Luce <joshuabluce@protonmail.com>
2020-06-29 08:52:12 -0500

    Logging and stuff

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const config = require('./config.json');
 const express = require('express');
+const chalk = require('chalk');
+const log = require('loglevel');
+const prefix = require('loglevel-plugin-prefix');
+const { ChatBadgeVersion } = require('twitch');
 const ChatClient = require('twitch-chat-client').default;
 const TwitchClient = require('twitch').default;
 
@@ -7,60 +11,80 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const client = TwitchClient.withCredentials(config.clientId, config.oauthToken);
-const chatbot = ChatClient.forTwitchClient(client);
-let channelData = new Map();
-chatbot.onRegister(() => {
-    config.channels.forEach(channel => {
-        chatbot.join(channel);
-    });
+const chatbot = ChatClient.forTwitchClient(client, {
+    channels: config.channels
 });
-chatbot.connect().then(() => {
-    config.channels.forEach(channel => {
+let channelData = new Map();
+
+const colors = {
+    TRACE: chalk.magenta,
+    DEBUG: chalk.cyan,
+    INFO: chalk.blue,
+    WARN: chalk.yellow,
+    ERROR: chalk.red
+};
+
+prefix.reg(log);
+log.enableAll();
+
+prefix.apply(log, {
+    format(level, name, timestamp) {
+        return `${chalk.gray(`[${timestamp}]`)} ${colors[level.toUpperCase()](level)}:`;
+    },
+});
+
+async function main() {
+    chatbot.onJoin((channelRaw) => {
+        let channel = channelRaw.replace('#', '');
+        log.info('Joined channel ' + channelRaw);
         channelData.set(channel, {
             counter: 0,
             lastMilestone: 0
         });
-        chatbot.onPrivmsg((channelRaw, user, message) => {
-            let channel = channelRaw.replace('#', '');
-            let counter = channelData.get(channel).counter;
-            let lastMilestone = channelData.get(channel).lastMilestone;
-            let regex = /[UuOo][Ww][UuOo]/g;
-            let match, matches = [];
-            if (user !== 'codathebot') {
-                if (message.startsWith('!owo') || message.startsWith('!uwu')) {
-                    chatbot.say(channel, 'OwO Counter: ' + counter);
-                } else {
-                    while ((match = regex.exec(message))) {
-                        matches.push(match.index);
+    });
+    chatbot.onPrivmsg((channelRaw, user, message) => {
+        let channel = channelRaw.replace('#', '');
+        let counter = channelData.get(channel).counter;
+        let lastMilestone = channelData.get(channel).lastMilestone;
+        let regex = /[UuOo][Ww][UuOo]/g;
+        let match, matches = [];
+        if (user !== chatbot.currentNick) {
+            if (message.startsWith('!owo') || message.startsWith('!uwu')) {
+                chatbot.say(channel, 'OwO Counter: ' + counter);
+            } else {
+                while ((match = regex.exec(message))) {
+                    matches.push(match.index);
+                }
+                if (matches.length > 0) {
+                    counter += matches.length;
+                    io.emit('counter', counter);
+                    let refreshedData = {
+                        counter: counter,
+                        lastMilestone: lastMilestone
                     }
-                    if (matches.length > 0) {
-                        counter += matches.length;
-                        io.emit('counter', counter);
-                        let refreshedData = {
-                            counter: counter,
-                            lastMilestone: lastMilestone
-                        }
-                        channelData.set(channel, refreshedData);
+                    channelData.set(channel, refreshedData);
+                }
+                let responseMilestone = 0;
+
+                config.milestones.forEach(milestone => {
+                    if (counter > milestone && counter > lastMilestone) {
+                        responseMilestone = milestone;
                     }
-                    let responseMilestone = 0;
-                    
-                    config.milestones.forEach(milestone => {
-                        if (counter > milestone && counter > lastMilestone) {
-                            responseMilestone = milestone;
-                        }
-                    });
-                    if (responseMilestone > 0 && responseMilestone > lastMilestone) {
-                        let refreshedData = {
-                            counter: counter,
-                            lastMilestone: responseMilestone
-                        }
-                        channelData.set(channel, refreshedData);
-                        chatbot.say(channel, 'We have reached ' + responseMilestone + ' OwOs!');
+                });
+                if (responseMilestone > 0 && responseMilestone > lastMilestone) {
+                    let refreshedData = {
+                        counter: counter,
+                        lastMilestone: responseMilestone
                     }
+                    channelData.set(channel, refreshedData);
+                    chatbot.say(channel, 'We have reached ' + responseMilestone + ' OwOs!');
                 }
             }
-        });
-        app.get('/' + channel, function(req, res) {
+        }
+    });
+    await chatbot.connect();
+    config.channels.forEach(channel => {
+        app.get('/' + channel, function (req, res) {
             res.sendFile(__dirname + '/public/index.html');
         });
         io.on('connection', (socket => {
@@ -69,9 +93,10 @@ chatbot.connect().then(() => {
         }))
         //chatbot.say(channel, 'Hewwo! OwO/UwU counter is now active! Summon me with !owo or !uwu.');
     });
-});
+}
+
+main();
 
 app.use(express.static('public/'));
 
 http.listen(8080);
-
